995bc91bb222309e4b61ee87bf55e42a97901636
Ville Marttila <ville.marttila@iki.fi>
2020-02-16 21:15:10 +0200

    Tehtävä 4.3 tehty

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const http = require('http')
-const mongoose = require('mongoose')
 const app = require('./app')
-const logger = require('./utils/logger')
 const config = require('./utils/config')
 
 const server = http.createServer(app)
