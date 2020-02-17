0c2d885671b00925754097f3b25ec412b6452c36
Ville Marttila <ville.marttila@iki.fi>
2020-02-17 02:21:17 +0200

    Tehtävä 4.5 tehty

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,7 +2,19 @@ const dummy = (blogs) => 1
 
 const totalLikes = (array) => array.reduce((sum, blog) => sum + blog.likes,0)
 
+const favoriteBlog = (array) => {
+  if (array.length === 0) return {}
+  array.sort((a, b) => b.likes - a.likes)
+  const favorite = array[0]
+  delete favorite.__v
+  delete favorite._id
+  delete favorite.url
+  return favorite
+}
+
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
