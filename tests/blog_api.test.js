2707e33349c5152ac44e9f999b92118912896921
Ville Marttila <ville.marttila@iki.fi>
2020-02-17 05:23:12 +0200

    Tehtävä 4.10 tehty

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -34,24 +34,62 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+describe('get', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('all the blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body.length).toBe(initialBlogs.length)
-})
+  test('all the blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body.length).toBe(initialBlogs.length)
+  })
 
-test('identifier property is called id', async () => {
-  const response = await api.get('/api/blogs')
-  response.body.forEach(blog => {
-    expect(blog.id).toBeDefined()
+  test('identifier property is called id', async () => {
+    const response = await api.get('/api/blogs')
+    response.body.forEach(blog => {
+      expect(blog.id).toBeDefined()
+    })
   })
 })
+
+describe('post', () => {
+  const blogToAdd = {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0
+  }
+
+  test('when a valid blog is added, blog count increases', async () => {
+    await api
+      .post('/api/blogs')
+      .send(blogToAdd)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    const response = await api.get('/api/blogs')
+    expect(response.body.length).toBe(initialBlogs.length + 1)
+  })
+
+  test('the title of the added blog is found', async () => {
+    await api
+      .post('/api/blogs')
+      .send(blogToAdd)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map((blog) => blog.title)
+    expect(titles).toContain(blogToAdd.title)
+  })
+})
+
+
+
+
 
 afterAll(() => {
   mongoose.connection.close()
