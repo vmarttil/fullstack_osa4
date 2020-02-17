06661f4a4af04240db62cbd79043a5dbb070840a
Ville Marttila <ville.marttila@iki.fi>
2020-02-17 19:42:01 +0200

    Tehtävä 4.17 tehty

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
@@ -11,14 +12,18 @@ blogsRouter.get('/:id', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body)
+  const user = await User.findOne({})
   if (!blog.likes) {
     blog.likes = 0
   }
+  blog.user = user._id
   try {
-    const result = await blog.save()
-    response.status(201).json(result.toJSON())
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    response.status(201).json(savedBlog.toJSON())
   } catch(exception) {
     next(exception)
   }
